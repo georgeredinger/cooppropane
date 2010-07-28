2d47f6b740fffaf2466371374a5fb786fbb54ae7
George Redinger <george@georgeredinger.com>
2010-07-28 16:31:17 -0700

    basic scraping tests pass

---
+++ lib/propane_scrape.rb
@@ -0,0 +1,16 @@
+def propane_scrape(page)
+   price=Array.new
+      doc = Nokogiri::HTML(page)
+      row=doc.xpath("//tr")
+      prices = Hash.new
+      row.each do |r|
+         if r.text.chomp.strip =~ /gallons/
+            q,g,p = r.text.chomp.split
+         end
+         if g == "gallons"
+            prices[q] = p
+         end
+      end
+      prices
+end
+
--- test/test_scrape.rb
+++ test/test_scrape.rb
@@ -1,28 +1,22 @@
 $:.unshift File.join(File.dirname(__FILE__),'..','lib')
 require 'test/unit'
 require 'rubygems'
-#require 'open-uri'
+require 'open-uri'
 require 'nokogiri'
 require 'find'
+require 'propane_scrape.rb'
 class ScrapeTest < Test::Unit::TestCase
-  def test_scrape
-    Find.find('./test/') do |f| 
-      price=Array.new
-      if f =~ /^.*\.html/
-        (2..7).each do |quantity|
-          path="/html/body/div[@class='Section1']/table[3][@class='MsoNormalTable']/tbody/tr/td[2]/table[@class='MsoNormalTable']/tbody/tr/td/div/table[@class='MsoNormalTable']/tbody/tr[1]/td/table[@class='MsoNormalTable']/tbody/tr/td[2]/div/table[@class='MsoNormalTable']/tbody/tr[#{quantity}]/td[3]/p[@class='MsoNormal']/span"
-          doc = Nokogiri::HTML(open(f))
-          price << doc.xpath(path).text
-        end
-      assert  price[1].to_f > 0.0
-      assert  price[2].to_f > 0.0
-      assert  price[3].to_f > 0.0
-      assert  price[4].to_f > 0.0
-      assert  price[5] == "Call"
-
-
+   def test_scrape_local
+      Find.find('./test/') do |f|
+         if f =~ /^.*\.html/
+           prices=propane_scrape(open(f).read)
+           puts prices.inspect 
+         end
       end
-          end
+   end
+  def test_scrape_url
+    page = open("http://www.co-openergy.org/prices.html")
+    prices=propane_scrape(page)
+    puts prices.inspect 
   end
 end
-
